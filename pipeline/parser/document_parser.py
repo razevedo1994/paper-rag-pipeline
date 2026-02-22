0998070da0461282aa4a2afc08ff9b34bd7600b4
Rodrigo <razevedo.contato@gmail.com>
2026-02-21 23:16:02 -0300

    feat: method to convert multiples source files

---
+++ pipeline/parser/document_parser.py
@@ -0,0 +1,7 @@
+from docling.document_converter import DocumentConverter
+
+def convert_documents(path: str):
+    converter = DocumentConverter()
+    result = converter.convert_all(source=path)
+    
+    return result
