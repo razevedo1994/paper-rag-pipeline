e2d8ccf7a6c2a9ff8be64c3370cadaa98c4beb1a
Rodrigo <razevedo.contato@gmail.com>
2026-02-26 07:40:03 -0300

    Update convert documents function

--- pipeline/parser/document_parser.py
+++ pipeline/parser/document_parser.py
@@ -3,8 +3,17 @@ from docling.document_converter import DocumentConverter
 
 def convert_documents(path: str):
     converter = DocumentConverter()
+
+    input_doc_paths = [
+        path / "2602.17831v1.pdf",
+        path / "2602.17831v1.pdf",
+        path / "2602.18230v1.pdf",
+        path / "2602.18372v1.pdf",
+        path / "2602.18374v1.pdf",
+    ]
+
     result = converter.convert_all(
-        source=path,
+        source=input_doc_paths,
         raises_on_error=False,
     )
 
