d84800e71a0f81e8cd2d68070365e570fe534561
Rodrigo <razevedo.contato@gmail.com>
2026-03-02 20:27:11 -0300

    update extract metadata function

--- pipeline/ingestion/metadata_extraction.py
+++ pipeline/ingestion/metadata_extraction.py
@@ -1,2 +1,26 @@
 import langextract as lx
 import textwrap
+from docling.document_converter import ConversionResult
+
+prompt = textwrap.dedent("""\
+""")
+
+examples = [
+    lx.data.ExampleData()
+]
+
+
+def extract_metadata(documents: ConversionResult) -> None:
+    for document in documents:
+        markdown_output = document.document.export_to_markdown()
+        first_pages = markdown_output[:6000]
+        extraction_result = lx.extract(
+            text_or_document=first_pages,
+            prompt_description=prompt,
+            examples=examples,
+            model_id="",
+        )
+
+        lx.io.save_annoted_documents(
+            [extraction_result], output_name="paper_metadata.jsonl"
+        )
