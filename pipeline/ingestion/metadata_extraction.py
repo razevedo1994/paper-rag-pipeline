80d0893e0e0815bc5cd6bc5ffb7f5a44f15783d8
Rodrigo <razevedo.contato@gmail.com>
2026-03-03 18:30:47 -0300

    Add metadata extraction examples

--- pipeline/ingestion/metadata_extraction.py
+++ pipeline/ingestion/metadata_extraction.py
@@ -9,7 +9,31 @@ Use exact text from the document.
 """)
 
 examples = [
-    lx.data.ExampleData()
+    lx.data.ExampleData(
+        text="[Re] Benchmarking LLM Capabilities in Negotiation through Scoreable Games\nJorge Carrasco Pollo\nInformatics Institute University of Amsterdam\nOriginal implementation: https://github.com/S-Abdelnabi/LLM-Deliberation",
+        extractions=[
+            lx.data.Extraction(
+                extraction_class="title",
+                extraction_text="[Re] Benchmarking LLM Capabilities in Negotiation through Scoreable Games",
+                attributes={},
+            ),
+            lx.data.Extraction(
+                extraction_class="author",
+                extraction_text="Jorge Carrasco Pollo",
+                attributes={},
+            ),
+            lx.data.Extraction(
+                extraction_class="affiliation",
+                extraction_text="Informatics Institute University of Amsterdam",
+                attributes={},
+            ),
+            lx.data.Extraction(
+                extraction_class="url",
+                extraction_text="https://github.com/S-Abdelnabi/LLM-Deliberation",
+                attributes={"type": "repository"},
+            ),
+        ],
+    )
 ]
 
 
