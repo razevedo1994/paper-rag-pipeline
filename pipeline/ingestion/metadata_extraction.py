6207df77a48735ca214e1e407f7768a9a0e23200
razevedo1994 <razevedo.contato@gmail.com>
2026-03-05 22:59:15 -0300

    Set model locally

--- pipeline/ingestion/metadata_extraction.py
+++ pipeline/ingestion/metadata_extraction.py
@@ -2,11 +2,13 @@ import langextract as lx
 import textwrap
 from docling.document_converter import ConversionResult
 
-prompt = textwrap.dedent("""\
+prompt = textwrap.dedent(
+    """\
 Extract metadata from this technical report including title, all authors, 
 affiliation and URLs.
 Use exact text from the document.
-""")
+"""
+)
 
 examples = [
     lx.data.ExampleData(
@@ -42,10 +44,11 @@ def extract_metadata(documents: ConversionResult) -> None:
         markdown_output = document.document.export_to_markdown()
         first_pages = markdown_output[:6000]
         extraction_result = lx.extract(
-            text_or_document=first_pages,
+            text_or_documents=first_pages,
             prompt_description=prompt,
             examples=examples,
-            model_id="",
+            model_id="gemma3",
+            model_url="http://localhost:11434",
         )
 
         lx.io.save_annoted_documents(
