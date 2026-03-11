37dd3a8ae582f41064b4831d348e7fe996fb2034
razevedo1994 <razevedo.contato@gmail.com>
2026-03-11 13:36:28 -0300

    Change model used and prompt to extract metadata

--- pipeline/ingestion/metadata_extraction.py
+++ pipeline/ingestion/metadata_extraction.py
@@ -7,6 +7,8 @@ prompt = textwrap.dedent(
 Extract metadata from this technical report including title, all authors, 
 affiliation and URLs.
 Use exact text from the document.
+
+"Return ONLY a valid JSON object. All values must be strings, numbers, or booleans. Never return null."
 """
 )
 
@@ -43,13 +45,20 @@ def extract_metadata(documents: ConversionResult) -> None:
     for document in documents:
         markdown_output = document.document.export_to_markdown()
         first_pages = markdown_output[:6000]
-        extraction_result = lx.extract(
-            text_or_documents=first_pages,
-            prompt_description=prompt,
-            examples=examples,
-            model_id="gemma3",
-            model_url="http://localhost:11434",
-        )
+
+        try:
+
+            extraction_result = lx.extract(
+                text_or_documents=first_pages,
+                prompt_description=prompt,
+                examples=examples,
+                model_id="gemma2:2b",
+                model_url="http://localhost:11434",
+            )
+
+        except Exception as e:
+            print(e)
+            raise
 
         lx.io.save_annoted_documents(
             [extraction_result], output_name="paper_metadata.jsonl"
